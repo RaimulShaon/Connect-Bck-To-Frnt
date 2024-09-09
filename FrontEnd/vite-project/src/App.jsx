9422fcf9c5a39c941d9b68350343a0e8fb81d9d2
RaimulShaon <raimulhasan@gmail.com>
2024-09-10 00:42:07 +0600

    Finaly Run to Front

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+
+useEffect(()=>{
+  axios.get('/api/jokes')
+  .then((response)=>{
+    setJokes(response.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+})
 
   return (
     <>
+
       <h1>This is my FrontEnd and Backend project</h1>
+      <p>Total JOKES: {jokes.length} </p>
+      {
+        jokes.map((joke, index)=>(
+            <div key={joke.id}> 
+            <h2>{joke.title}</h2>
+            <p>{joke.content}</p>
+            </div>
+        ))
+      }
+
     </>
   )
 }
--- backend/server.js
+++ backend/server.js
@@ -9,34 +9,34 @@ app.get('/',(req, res)=>{
     res.send('server is ready');
 });
 
-app.get('/jokes',(req, res)=>{
+app.get('/api/jokes',(req, res)=>{
 const jokes = [
     {
         id : 1,
-        title: 'jokes num one',
+        title: 'Jokes num One',
         content : 'ekoda ek chilo gadha'
 
     },
     {
         id : 2,
-        title: 'jokes num Two',
+        title: 'Jokes num Two',
         content : 'ekoda ek chilo gadha 2'
 
     },
     {
         id : 3,
-        title: 'jokes num Three',
+        title: 'Jokes num Three',
         content : 'ekoda ek chilo gadha 3'
 
     },
     {
         id : 4,
-        title: 'jokes num Four',
+        title: 'Jokes num Four',
         content : 'ekoda ek chilo gadha 4'
 
     }
 ];
-    res.send(jokes)
+    res.send(jokes);
 });
 
 const port = process.env.PORT||3000;
